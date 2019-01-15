d5a152d4b2b388f3b97138ab0d4afdbb31da20ac
A Glazer <avigayilglazer@gmail.com>
2019-01-15 14:31:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,7 +190,7 @@ def player_numbers(team_name)
 
 
         if attribute == :team_name && data == team_name
-          team_info[:players].each |name, stats|
+          team_info[:players].each do |name, stats|
 binding.pry
         #data.each do |name, stats|
           #if data == team_name
